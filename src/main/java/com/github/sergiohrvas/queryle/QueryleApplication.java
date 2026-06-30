f4a8c4d4de988e038a8eeb13d26dd959de899ef3
Sergio Hervás <sergiohervas18@gmail.com>
2026-06-30 20:45:37 +0200

    First commit: Initial Spring Boot project

---
+++ src/main/java/com/github/sergiohrvas/queryle/QueryleApplication.java
@@ -0,0 +1,13 @@
+package com.github.sergiohrvas.queryle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sergiohrvas/queryle/QueryleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sergiohrvas.queryle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
