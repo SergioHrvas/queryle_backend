f86ddc7630ce8072566aa7288b907b872b0bccfa
Sergio Hervás <sergiohervas18@gmail.com>
2026-07-05 16:37:45 +0200

    feat: player stats controller, dto and mapper closes #21

---
+++ src/main/java/com/github/sergiohrvas/queryle/challenges/infrastructure/controllers/PlayerStatsController.java
@@ -0,0 +1,31 @@
+package com.github.sergiohrvas.queryle.challenges.infrastructure.controllers;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.github.sergiohrvas.queryle.challenges.application.dtos.PlayerStatsResponseDTO;
+import com.github.sergiohrvas.queryle.challenges.application.mappers.PlayerStatsMapper;
+import com.github.sergiohrvas.queryle.challenges.application.usecases.GetPlayerStatsUseCase;
+import com.github.sergiohrvas.queryle.challenges.domain.models.PlayerStats;
+
+@RestController
+@RequestMapping("/api/v1/player-stats")
+public class PlayerStatsController {
+    private final GetPlayerStatsUseCase getPlayerStatsUseCase;
+
+    public PlayerStatsController(GetPlayerStatsUseCase getPlayerStatsUseCase) {
+        this.getPlayerStatsUseCase = getPlayerStatsUseCase;
+    }
+
+    @GetMapping
+    public ResponseEntity<PlayerStatsResponseDTO> getPlayerStats(@RequestHeader UUID playerId) {
+        PlayerStats playerStats = getPlayerStatsUseCase.execute(playerId);
+        return ResponseEntity.ok(PlayerStatsMapper.toDTO(playerStats));
+    }
+}
